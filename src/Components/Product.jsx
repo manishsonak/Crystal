184ec91a4445819d425d6b434648996cdbf4367d
manishsonak <manishsonak9@gmail.com>
2024-10-01 20:28:22 +0530

    adding product view all functionlity

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -2,32 +2,29 @@ import { useState } from "react";
 import Card from "./Card";
 
 function Product() {
+  const Filter = [
+    {
+      filter: "All Product",
+      sort: "Best Selling",
+    },
+    {
+      filter: "Crystals",
+      sort: "On Sale",
+    },
+    {
+      filter: "Jewelry",
+      sort: "New Arrivals",
+    },
+    {
+      filter: "Phone Accessories",
+    },
+    {
+      filter: "Limited Edition",
+      sort: "Recomended",
+    },
+  ];
 
-
-    const Filter=[
-        {
-            filter:'All Product',
-            sort:'Best Selling'
-        },
-        {
-            filter:'Crystals',
-            sort:'On Sale'
-        },
-        {
-            filter:'Jewelry',
-            sort:'New Arrivals'
-        },
-        {
-            filter:'Phone Accessories',
-        },
-        {
-            filter:'Limited Edition',
-            sort:'Recomended'
-        },
-    ]
-
-         const [isView, setIsView] = useState(false);
-
+  const [isView, setIsView] = useState(false);
 
   return (
     <div className="w-full min-h-screen mt-10 lg:mt-0">
@@ -42,12 +39,11 @@ function Product() {
             name="filter"
             id="filter"
           >
-            {
-                Filter.map((item, index) => (
-                    <option key={index} value={item.filter}>{item.filter}</option>
-                ))
-            }
-    
+            {Filter.map((item, index) => (
+              <option key={index} value={item.filter}>
+                {item.filter}
+              </option>
+            ))}
           </select>
         </div>
         <div className="flex items-center gap-1">
@@ -57,38 +53,93 @@ function Product() {
             name="filter"
             id="filter"
           >
-             {
-                Filter.map((item, index) => (
-                   item.sort && <option key={index} value={item.sort}>{item.sort}</option>
-                ))
-            }
+            {Filter.map(
+              (item, index) =>
+                item.sort && (
+                  <option key={index} value={item.sort}>
+                    {item.sort}
+                  </option>
+                )
+            )}
           </select>
         </div>
       </div>
-                {/* Products */}
+      {/* Products */}
 
-            <div className={`mt-3  lg:mx-auto overflow-x-auto min-h-[80vh]  max-h-[${isView?"auto":'110vh'}] justify-center lg:justify-start lg:max-w-[1440px] flex flex-wrap gap-3 py-3 px-6 lg:overflow-hidden `}>
-                <Card image='../assets/item.png' name="Crystal Agate Phone Grip" price="18.99$"/>
-                <Card image='../assets/item.png' name="Crystal Agate Phone Grip" price="18.99$"/>
-                <Card image='../assets/item.png' name="Crystal Agate Phone Grip" price="18.99$"/>
-                <Card image='../assets/item.png' name="Crystal Agate Phone Grip" price="18.99$"/>
-                <Card image='../assets/item.png' name="Crystal Agate Phone Grip" price="18.99$"/>
-                <Card image='../assets/item.png' name="Crystal Agate Phone Grip" price="18.99$"/>
-                <Card image='../assets/item.png' name="Crystal Agate Phone Grip" price="18.99$"/>
-                <Card image='../assets/item.png' name="Crystal Agate Phone Grip" price="18.99$"/>
-                <Card image='../assets/item.png' name="Crystal Agate Phone Grip" price="18.99$"/>
-                <Card image='../assets/item.png' name="Crystal Agate Phone Grip" price="18.99$"/>
-                <Card image='../assets/item.png' name="Crystal Agate Phone Grip" price="18.99$"/>
-            </div>
+      <div
+        className={`mt-3  lg:mx-auto overflow-y-auto   ${
+          isView ? "lg:max-h-[auto]" : "lg:max-h-[115vh]"
+        } justify-center lg:justify-start lg:max-w-[1440px] flex flex-wrap gap-3 py-3 px-6 lg:overflow-hidden `}
+      >
+        <Card
+          image="../assets/item.png"
+          name="Crystal Agate Phone Grip"
+          price="18.99$"
+        />
+        <Card
+          image="../assets/item.png"
+          name="Crystal Agate Phone Grip"
+          price="18.99$"
+        />
+        <Card
+          image="../assets/item.png"
+          name="Crystal Agate Phone Grip"
+          price="18.99$"
+        />
+        <Card
+          image="../assets/item.png"
+          name="Crystal Agate Phone Grip"
+          price="18.99$"
+        />
+        <Card
+          image="../assets/item.png"
+          name="Crystal Agate Phone Grip"
+          price="18.99$"
+        />
+        <Card
+          image="../assets/item.png"
+          name="Crystal Agate Phone Grip"
+          price="18.99$"
+        />
+        <Card
+          image="../assets/item.png"
+          name="Crystal Agate Phone Grip"
+          price="18.99$"
+        />
+        <Card
+          image="../assets/item.png"
+          name="Crystal Agate Phone Grip"
+          price="18.99$"
+        />
+        <Card
+          image="../assets/item.png"
+          name="Crystal Agate Phone Grip"
+          price="18.99$"
+        />
+        <Card
+          image="../assets/item.png"
+          name="Crystal Agate Phone Grip"
+          price="18.99$"
+        />
+        <Card
+          image="../assets/item.png"
+          name="Crystal Agate Phone Grip"
+          price="18.99$"
+        />
+      </div>
 
-            {/* View More */}
+      {/* View More */}
 
-            <div className="items-center justify-center hidden h-40 lg:flex">
-            <button onClick={()=>{
-                setIsView(!isView)
-            }} className="my-4  w-[134px] h-[48px] rounded-2xl  font-semibold hover:shadow-lg  hover:text-white   border-2 border-[#75CCEB] text-[#75CCEB] hover:bg-gradient-to-r from-[#75CCEB] to-[#6FB4FF] py-2 px-4 transition duration-300">{isView?"View Less":"View All"}</button>
-            </div>
-
+      <div className="items-center justify-center hidden h-40 lg:flex">
+        <button
+          onClick={() => {
+            setIsView(!isView);
+          }}
+          className="my-4  w-[134px] h-[48px] rounded-2xl  font-semibold hover:shadow-lg  hover:text-white   border-2 border-[#75CCEB] text-[#75CCEB] hover:bg-gradient-to-r from-[#75CCEB] to-[#6FB4FF] py-2 px-4 transition duration-300"
+        >
+          {isView ? "View Less" : "View All"}
+        </button>
+      </div>
     </div>
   );
 }
